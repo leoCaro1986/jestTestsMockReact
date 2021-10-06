410020848292199d802dfa62de400351e1cf1fe1
Leonardo <leocv0205@gmail.com>
2021-10-05 19:25:38 -0500

    Se crea el repositorio de jest_Mock

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+
+import * as app from "./app";
+import * as math from "./math";
+
+math.add = jest.fn();
+math.subtract = jest.fn();
+
+test("calls math.add", () => {
+  app.doAdd(1, 2);
+  expect(math.add).toHaveBeenCalledWith(1, 2);
+});
+
+test("calls math.subtract", () => {
+  app.doSubtract(1, 2);
+  expect(math.subtract).toHaveBeenCalledWith(1, 2);
+});
+
+
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
